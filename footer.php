5e3f791bc38c123f4ed36f64ab6f886aa64c801a
Kazek <kazek@nurtureagency.com>
2018-07-19 21:49:01 +0200

    Pierwszy commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage kat-refining
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+		
